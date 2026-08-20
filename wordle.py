fc4a719b0262844fca8323a6afb3167f9209abf9
Josna Johnson <josnajohnson01@gmail.com>
2026-08-20 15:48:27 +0530

    Initial commit - word game code added

--- wordle.py
+++ wordle.py
@@ -1,41 +1,35 @@
 from letter_state import LetterState
 
-class wordle:
-
+class Wordle:
     MAX_ATTEMPTS = 6
     WORD_LENGTH = 5
 
     def __init__(self, secret: str):
-          self.secret: str = secret
-          self.attempts = []
-     
-    def attempts(self, word: str):
-         word = word.upper()
-         self.attempts.append(word)
+        self.secret: str = secret.upper()
+        self.attempts: list[str] = []
 
-    def guess(self, word: str):  
+    def attempt(self, word: str):
+        self.attempts.append(word.upper())
+
+    def guess(self, word: str) -> list[LetterState]:
         word = word.upper()
-        result = [] 
-
-        for i in range(self. WORD_LENGTH):
-             character = word[i]
-             letter = LetterState(character)
-             letter.is_in_word = character in self.secret
-             letter.is_in_word = character == self.secret[i]
-             result.append(letter)
+        result = []
+        for i in range(self.WORD_LENGTH):
+            character = word[i]
+            letter = LetterState(character)
+            letter.is_in_word = character in self.secret
+            letter.is_in_position = character == self.secret[i]
+            result.append(letter)
         return result
 
     @property
-    def is_solved(self):
-         return len(self.attempts) > 0 and self.attempts[-1] == self.secret
+    def is_solved(self) -> bool:
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
 
     @property
     def remaining_attempts(self) -> int:
-         return self.MAX_ATTEMPTS - len(self.attempts)
-    
-    @property
-    def can_attempts(self):
-         return self.remaining_attempts > 0 < self.MAX_ATTEMPTS and not self.is_solved
-         
+        return self.MAX_ATTEMPTS - len(self.attempts)
 
-    
+    @property
+    def can_attempt(self) -> bool:
+        return self.remaining_attempts > 0 and not self.is_solved
